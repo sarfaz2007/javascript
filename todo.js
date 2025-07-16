ef90cabadbb7920a8deab2f92f0b04855b69ad21
sarfaz2007 <sarfazsarfaz646@gmail.com>
2025-07-16 23:08:22 +0530

    16/7/25 nght

---
+++ todo.js
@@ -0,0 +1,33 @@
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = [];
+
+todoForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+})
+
+function addTodo(){
+    const todoText = todoInput.ariaValueMax.trim();
+    if(todoText.length > 0){
+        allTodos.push(ttodotext);
+        createTodoItem(todotext);
+        todoInput.value = "";
+
+    }
+}
+
+function updateTdodList(){
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIntex)=>{
+        
+    })
+
+}
+function createTodoItem(todo){
+    const todoLI = document.createElement("li");
+    todoLi.innerteText = todo;
+    todoListUL.append(todoLI);
+}
