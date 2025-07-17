2d1ed8e1654ccb31704790bbefd9f3f3bf755105
sarfaz2007 <sarfazsarfaz646@gmail.com>
2025-07-17 23:07:27 +0530

    17/7/25 night

--- todo.js
+++ todo.js
@@ -10,18 +10,16 @@ todoForm.addEventListener('submit',function(e){
     console.log("hyfutfy");
     addTodo();
 })
-
-
-
-
-
 function addTodo(){
     const todoText = todoInput.value.trim();
     console.log(todoText);
     if(todoText.length > 0){
-
+        const todoObject = {
+            text : todoText,
+            completed: false
+        }
         console.log("inside if");
-        allTodos.push(todoText);
+        allTodos.push(todoObject);
         updateTodoList();
         saveTodos();
         console.log(saveTodos);
@@ -36,6 +34,7 @@ function createTodoItem(todo, todoIndex) {
     console.log(todoIndex);
     const todoId = "todo" + todoIndex;
     const todoLI = document.createElement("li");
+    const todoText = todo.text
     todoLI.className = "todo";
     todoLI.innerHTML = `
         <input type="checkbox" id="${todoId}">
@@ -44,7 +43,7 @@ function createTodoItem(todo, todoIndex) {
             <!-- <i class="fa-solid fa-square-check"></i> -->
         </label>
         <label for="${todoId}" class="todo-text">
-            ${todo}
+            ${todoText}
         </label>
         <button class="delete-button">
             <!-- <img src="icons/delete_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="svg image"> -->
@@ -53,12 +52,19 @@ function createTodoItem(todo, todoIndex) {
     `
     const deleteButton = todoLI.querySelector(".delete-button")
     deleteButton.addEventListener("click", ()=>{
-      deleteTodoItem(todoindex)
+      deleteTodoItem(todoIndex)
     });
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].comleted = checkbox.checked;
+    })
     return todoLI;
 } 
 function deleteTodoItem(todoindex){
     allTodos = allTodos.filter((_, i)=> i !== todoindex);
+    saveTodos();
+    updateTodoList();
+
 }
 
 function saveTodos(){
